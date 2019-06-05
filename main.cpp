6e9568fa45cbe7f90e043b192cc7b57266d89338
elliotgoodwin <47867633+elliotgoodwin@users.noreply.github.com>
2019-06-05 21:01:36 +0100

    Create main.cpp
    A game of object-oriented (OO) chess in C++

---
+++ main.cpp
@@ -0,0 +1,27 @@
+// ChessGame.cpp
+// A game of object-oriented (OO) chess in C++
+// Elliot Goodwin
+// 10/05/2019
+
+// Includes and headers
+
+// C++ STL includes
+#include <iostream>
+#include <string>
+#include <sstream>
+
+// Custom built headers for a game of OO chess
+#include "ChessBoard.h"
+#include "ChessEngine.h"
+
+// Declare namespace
+using namespace Chess;
+
+int main() {
+
+	// Play a game of chess!
+  // playGame() definition at l. 1067-1146 in GameEngine.cpp
+  playGame();
+
+	return 0;
+}
